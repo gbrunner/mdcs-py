c700203024644da885100aa33c44021678c1ed8d
Greg Brunner <gbrunner@esri.com>
2020-11-02 14:22:49 -0600

    added the scrip to build the archive

---
+++ scripts/INSTRUCTIONS.md
@@ -0,0 +1,40 @@
+# Instructions for building the mosaic of all achived GPM MRF files
+
+## Create OMD folder
+1. In the mdcs-py folder, create a new folder named **0MD**.
+
+## Set Paramters in build_archive.py
+1. Change workspace to the location of the MRF files
+```
+workspace = 'D:/DATA/NASA_DAILY/mrf-archive-2019'
+```
+2. Point to your config file in /Parameter/Config/gpm.xml
+```
+config = 'C:/PROJECTS/NASA/gpm-newmdcs/Parameter/Config/gpm.xml'
+```
+3. Define the mosaic dataset that you will be creating. We recommend creating it in the **0MD** folder.
+```
+mastermd = 'C:/PROJECTS/NASA/gpm-newmdcs/0MD/Master.gdb/GPM_2019'
+```
+4. Define the table that you will create in order to populate the mosaic dataset. We recommend creating it in **0MD** folder.
+```
+outputsrcFC = 'C:/PROJECTS/NASA/gpm-newmdcs/0MD/inputtable.gdb/gpm_2019'
+ ```
+
+## Update parameters in Config/gpm.xml
+
+1. Open **gpm.xml** in the **Config** folder.
+2. Set ```<WorkspacePath>c:\PROJECTS\NASA\gpm-newmdcs\0MD</WorkspacePath>``` to point to the folder that will hold your mosaic dataset.
+3. Set ```<Geodatabase>Master.gdb</Geodatabase>``` to the name of your output mosaic.
+4. Set ```<processing_templates>C:\PROJECTS\NASA\gpm-newmdcs\Parameter\RasterFunctionTemplates\GPM_Stretch_DRA.rft.xml</processing_templates>``` to point to your **GPM_Stretch_DRA raster function**.
+5. These should be the only changes you need to make.
+
+## Update MDCS_UC.py
+
+1. Open **MDCS_UC.py**.
+2. Go to line 200. It will read:
+```<DataFile>c:/mrfcache/gpm/{4}.mrf_cache</DataFile><IndexFile>c:/mrfcache/gpm/{4}.mrf_cache</IndexFile></Raster>\n' ```
+3. Change **c:/mrfcache** to be a folder that exists on your system. We recommend that you create the **mrfcache** folder on your C, D, or Z drive.
+4. Go to line 222: It will read:
+```<DataFile>c:/mrfcache/gpm/{2}.mrf_cache</DataFile><IndexFile>c:/mrfcache/gpm/{2}.mrf_cache</IndexFile></Raster>\n'```
+5. Again, change **c:/mrfcache** to be a folder that exists on your system. We recommend that you create the **mrfcache** folder on your C, D, or Z drive.
---
+++ scripts/build_archive.py
@@ -0,0 +1,70 @@
+import os
+import datetime
+import json
+import arcpy
+import MDCS
+
+def replace_str_index(text,index=0,replacement=''):
+    return '%s%s%s'%(text[:index],replacement,text[index+1:])
+
+## Edit these paramters
+workspace = 'D:/DATA/NASA_DAILY/mrf-archive-2019'
+config = 'C:/PROJECTS/NASA/mdcs-py/Parameter/Config/gpm.xml'
+mastermd = 'C:/PROJECTS/NASA/mdcs-py/0MD/Master.gdb/GPM_2019'
+outputsrcFC = 'C:/PROJECTS/NASA/mdcs-py/0MD/inputtable.gdb/gpm_2019'
+interval = '30'
+intervalunits = 'minutes'
+bucket = '#'
+infolder = '#'
+startslice = '0'
+endslice = '48'
+
+total_start = datetime.datetime.now()
+
+walk = arcpy.da.Walk(workspace, topdown=True, datatype="RasterDataset")
+
+for dirpath, dirnames, filenames in walk:
+    if len(filenames) > 0:
+        start = datetime.datetime.now()
+        filename = filenames[0]
+        inputmrf = os.path.join(dirpath, filename)
+
+        for root, dirs, files in os.walk(dirpath):
+            for file in files:
+                if file.endswith(".json"):
+                    print(file)
+                    with open(os.path.join(dirpath, file)) as f:
+                        date_config = json.load(f)
+                        startdate = replace_str_index(
+                            date_config[str(int(startslice))], 10, "T").replace("-", "/")+".00"
+                        enddate = replace_str_index(
+                            date_config[str(int(endslice)-1)], 10, "T").replace("-", "/")+".00"
+                        print(startdate)
+                        print(enddate)
+                        MDCS.main(14,
+                              ['-i:' + config,
+                              '-p:' + inputmrf + '$inmrf',
+                              '-p:' + outputsrcFC + '$outFC',
+                              '-p:' + startdate + '$sdate',
+                              '-p:' + enddate + '$edate',
+                              '-p:' + interval + '$interval',
+                              '-p:' + intervalunits + '$intervalunits',
+                              '-p:' + bucket + '$bucket',
+                              '-p:' + infolder + '$infolder',
+                              '-p:' + startslice + '$sslice',
+                              '-p:' + endslice + '$eslice',
+                              '-m:' + mastermd,
+                              '-s:' + outputsrcFC,
+                              '-c:buildSRCTable']) #+CM+RI+AR+SS+AI+SP
+
+        finish = datetime.datetime.now()
+        print(f"finish: {finish}, runtime: {finish - start} ")
+
+MDCS.main(4,
+              ['-i:' + config,
+              '-m:' + mastermd,
+              '-s:' + outputsrcFC,
+              '-c:CM+RI+AR+SS+AI+SP'])
+
+total_finish = datetime.datetime.now()
+print(f"finish: {total_finish}, runtime: {total_finish - total_start} ")
